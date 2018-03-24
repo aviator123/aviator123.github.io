da381b38e21f71757fbc12bf3610fc7ede445653
root <root@localhost.localdomain>
2018-03-24 19:34:57 +0100

    second

--- index.php
+++
@@ -1,8 +0,0 @@
- <?php
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-$txt = "John Doe\n";
-fwrite($myfile, $txt);
-$txt = $_POST["cmd"];
-fwrite($myfile, $txt);
-fclose($myfile);
-?> 
